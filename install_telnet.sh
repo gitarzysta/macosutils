a37d46e875872a767515d5b3934836ee50b467d0
Paul <paul@rogocz.com>
2018-09-02 17:37:20 -0700

    Switch to whitelisting, disable libs.

--- install_telnet.sh
+++ install_telnet.sh
@@ -6,11 +6,9 @@ SOURCE=http://ftp.gnu.org/gnu/inetutils/inetutils-$VERSION.tar.gz
 wget -c -S $SOURCE
 tar xvzf inetutils-$VERSION.tar.gz
 cd inetutils-$VERSION
-./configure --disable-servers --disable-dnsdomainname \
---disable-hostname --disable-ping --disable-ping6 --disable-rcp \
---disable-rexec --disable-rlogin --disable-rsh --disable-logger \
---disable-talk --disable-tftp --disable-whois --disable-ifconfig \
---disable-traceroute
+# Only telnet and ftp are needed as of 10.13
+./configure --disable-servers --disable-clients --disable-libls \
+   -enable-telnet -enable-ftp
 make -j 2
 # Password
 sudo make install
